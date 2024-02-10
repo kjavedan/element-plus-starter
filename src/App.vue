017a00a87bf0a715e7bd2772f13ebd43fd92b182
Khaled Javedan <khaledjavedan76@gmail.com>
2024-02-10 12:16:01 +0400

    init

---
+++ src/App.vue
@@ -0,0 +1,11 @@
+<template>
+  <div>
+    <el-config-provider namespace="ep">
+      <RouterView />
+    </el-config-provider>
+  </div>
+</template>
+
+<script setup lang="ts"></script>
+
+<style scoped></style>
---
+++ src/pages/home/index.vue
@@ -0,0 +1,11 @@
+<template>
+  <div>
+    <el-button type="primary" @click="toggleDark()">switch</el-button>
+  </div>
+</template>
+
+<script setup>
+import { toggleDark } from "~/composables";
+</script>
+
+<style lang="scss" scoped></style>
